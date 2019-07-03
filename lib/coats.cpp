acf66486e55b444ddcaa4c56fba5453c914cd58b
Pat Chizek <pchizek@calpoly.edu>
2019-07-03 11:20:31 -0700

    Added status and command capabilities

--- lib/coats.cpp
+++ lib/coats.cpp
@@ -2,10 +2,12 @@
 
 #include "coats.h"
 
-coats::coats(uint16_t ending, bool counterEnable)
+coats::coats(uint16_t ending, bool statusEnable)
 {
 	endString = ending;
-	timerEnable = counterEnable;
+
+  // Enable status Tlm
+  
 }
 
 /*
@@ -26,95 +28,185 @@ void coats::serialInit(Serial_& serialInst, long baud)
   
 }
 
+/* Send a telemetry packet */
 void coats::serialWriteTlm(HardwareSerial& serialInst, uint8_t id){
-
-  if (timerEnable) {
-    uint16_t rtc = (uint32_t)(millis());
-    SerialCOATS._hw->write(id);
-    SerialCOATS._hw->write(rtc);
-    SerialCOATS._hw->write(rtc >> 8);
-    SerialCOATS._hw->write(rtc >> 16);
-    SerialCOATS._hw->write(rtc >> 24);
-    SerialCOATS._hw->write((byte *)packetPointers[id], packetSizes[id]);
-    SerialCOATS._hw->write(endString >> 8);
-    SerialCOATS._hw->write(endString); 
-  }
   
-  else{
-    SerialCOATS._hw->write(id);
-    SerialCOATS._hw->write((byte *)packetPointers[id], packetSizes[id]);
-    SerialCOATS._hw->write(endString >> 8);
-    SerialCOATS._hw->write(endString); 
-  }
+  SerialCOATS._hw->write(id);
+  SerialCOATS._hw->write((byte *)packetPointers[id], packetSizes[id]);
+  SerialCOATS._hw->write(endString >> 8);
+  SerialCOATS._hw->write(endString); 
   
 }
-
 
 void coats::serialWriteTlm(Serial_& serialInst, uint8_t id){
+  
+	SerialCOATS._sw->write(id);
+	SerialCOATS._sw->write((byte *)packetPointers[id], packetSizes[id]);
+	SerialCOATS._sw->write(endString >> 8);
+  SerialCOATS._sw->write(endString); 
 
-	if (timerEnable) {
-		uint16_t rtc = (uint32_t)(millis());
-		SerialCOATS._sw->write(id);
-		SerialCOATS._sw->write(rtc);
-		SerialCOATS._sw->write(rtc >> 8);
-    SerialCOATS._sw->write(rtc >> 16);
-    SerialCOATS._sw->write(rtc >> 24);
-		SerialCOATS._sw->write((byte *)packetPointers[id], packetSizes[id]);
-		SerialCOATS._sw->write(endString >> 8);
-		SerialCOATS._sw->write(endString); 
-	}
-	
-	else{
-		SerialCOATS._sw->write(id);
-		SerialCOATS._sw->write((byte *)packetPointers[id], packetSizes[id]);
-		SerialCOATS._sw->write(endString >> 8);
-		SerialCOATS._sw->write(endString); 
-	}
-	
 }
 
-/* Adds a data structure */ 
+void coats::serialWriteTlm(HardwareSerial& serialInst, uint8_t id, uint32_t timeStamp){
+
+  SerialCOATS._hw->write(id);
+  SerialCOATS._hw->write(timeStamp);
+  SerialCOATS._hw->write(timeStamp >> 8);
+  SerialCOATS._hw->write(timeStamp >> 16);
+  SerialCOATS._hw->write(timeStamp >> 24);
+  SerialCOATS._hw->write((byte *)packetPointers[id], packetSizes[id]);
+  SerialCOATS._hw->write(endString >> 8);
+  SerialCOATS._hw->write(endString); 
+
+}
+
+void coats::serialWriteTlm(Serial_& serialInst, uint8_t id, uint32_t timeStamp){
+
+  SerialCOATS._sw->write(id);
+  SerialCOATS._sw->write(timeStamp);
+  SerialCOATS._sw->write(timeStamp >> 8);
+  SerialCOATS._sw->write(timeStamp >> 16);
+  SerialCOATS._sw->write(timeStamp >> 24);
+  SerialCOATS._sw->write((byte *)packetPointers[id], packetSizes[id]);
+  SerialCOATS._sw->write(endString >> 8);
+  SerialCOATS._sw->write(endString); 
+
+}
+
+
+/* Send a Status Packet */
+void coats::serialWriteStat(HardwareSerial& serialInst, uint8_t statMsg){
+
+  SerialCOATS._hw->write(STATUS_ID);
+  SerialCOATS._hw->write(statMsg);
+  SerialCOATS._hw->write(endString >> 8);
+  SerialCOATS._hw->write(endString); 
+
+}
+
+void coats::serialWriteStat(Serial_& serialInst, uint8_t statMsg){
+
+  SerialCOATS._sw->write(STATUS_ID);
+  SerialCOATS._sw->write(statMsg);
+  SerialCOATS._sw->write(endString >> 8);
+  SerialCOATS._sw->write(endString); 
+
+}
+
+/* Reads a command from the serial port given
+ * 
+ * FORMAT: 
+ * | Start | CMD | CMD Value | Checksum | 
+ */
+
+void coats::serialParseCmd(HardwareSerial& serialInst){
+
+  // Check for input
+  if (SerialCOATS._hw->available()){
+    
+    uint8_t input[4];
+
+    // Read from buffer
+    SerialCOATS._hw->readBytes(input,5);
+
+    
+    // Read command checksum (XOR)
+    uint8_t cs_calc = input[1]^input[2];
+
+    if(input[3]==cs_calc){
+
+      // Send parameter to callback function
+      cmdCallbackPtr[input[1]](input[2]);
+        
+    }
+
+    else{
+
+      // Inform of packet garbled
+        
+    }
+      
+  }
+  
+}
+  
+void coats::serialParseCmd(Serial_& serialInst){
+
+  // Check for input
+  if (SerialCOATS._sw->available()){
+    
+    char input[4];
+
+    // Read from buffer
+    SerialCOATS._sw->readBytes(input,4);
+
+    
+    // Read command checksum (XOR)
+    uint8_t cs_calc = input[1]^input[2];
+
+    if(input[3]==cs_calc){
+
+      // Send parameter to callback function
+      cmdCallbackPtr[input[1]](input[2]);
+        
+    }
+
+    else{
+
+      // Inform of packet garbled
+        
+    }
+      
+  }
+
+}
+  
+
+/* Adds a new telemetry packet */ 
 void coats::addTlm(uint8_t id, uint32_t * data){
 	
 	packetSizes[id] = sizeof(*data);
-  SerialUSB.println(packetSizes[id]);
+  //SerialUSB.println(packetSizes[id]);
 	packetPointers[id] = data;
 
 }
 
+/* Adds a command */
+void coats::addCmd(uint8_t id, cmdCallback callback){
+  cmdCallbackPtr[id] = callback;
+}
+
+
 /* Outputs a packet as a string */
 void coats::buildTlm(uint8_t id, String packet){
 	size_t dataSize = packetSizes[id];
-	
-	if (timerEnable) {
-		uint16_t rtc = (uint16_t)(millis());
-		packet[0] = id;
-		packet[1] = rtc;
-		packet[2] = (rtc >> 8);
-    packet[3] = (rtc >> 16);
-    packet[4] = (rtc >> 24);
-		for (int i=0;i<dataSize;i++)
-		{
-			packet[i+5] = *(packetPointers[id] + 8*i);
-		}
 
-		packet[dataSize+5] = (endString >> 8);
-		packet[dataSize+6] = (endString);
+	packet[0] = id;
+	for (int i=0;i<dataSize;i++)
+	{
+		packet[i+1] = *(packetPointers[id] + 8*i);
 	}
-	
-	else{
-		packet[0] = id;
-		for (int i=0;i<dataSize;i++)
-		{
-			packet[i+1] = *(packetPointers[id] + 8*i);
-		}
-
-		packet[dataSize+1] = (endString >> 8);
-		packet[dataSize+2] = (endString); 
-	}
+	packet[dataSize+1] = (endString >> 8);
+	packet[dataSize+2] = (endString); 
 
 }
 
-/* Generate checksum */
-uint8_t coats::xor_checksum(){
+void coats::buildTlm(uint8_t id, String packet, uint32_t timeStamp){
+  size_t dataSize = packetSizes[id];
+  
+  packet[0] = id;
+  packet[1] = timeStamp;
+  packet[2] = (timeStamp >> 8);
+  packet[3] = (timeStamp >> 16);
+  packet[4] = (timeStamp >> 24);
+  for (int i=0;i<dataSize;i++)
+  {
+    packet[i+5] = *(packetPointers[id] + 8*i);
+  }
+
+  packet[dataSize+5] = (endString >> 8);
+  packet[dataSize+6] = (endString);
+
+
+
 }
--- lib/coats.h
+++ lib/coats.h
@@ -8,32 +8,54 @@
 #define CNT_ENABLE (1)
 #define CNT_DISABLE (0)
 
+
+#define STATUS_ID 129
+
+typedef void (*cmdCallback)(uint8_t cmd);
+
 class coats
 {
   
 	public:
 	
-		coats(uint16_t ending, bool counterEnable);
-	
+		coats(uint16_t ending, bool statusEnable);
+
+    /*
+     *  Setup functions
+     */ 
+     
+    void addTlm(uint8_t id, uint32_t* data);
+
+    void addCmd(uint8_t id, cmdCallback callback);
+     
+    void buildTlm(uint8_t id, String packet);
+
+    void buildTlm(uint8_t id, String packet, uint32_t timeStamp);
+  
     /*
      *  UART Interface
      */
-     
+
+    // Initialize a software or hardware serial connection
     void serialInit(HardwareSerial& serialInst,long baud);
-    
     void serialInit(Serial_& serialInst, long baud);
 
+    // Write a status packet to the serial interface
+    void serialWriteStat(HardwareSerial& serialInst, uint8_t statMsg);
+    void serialWriteStat(Serial_& serialInst, uint8_t statMsg);
+
+    // Write a telemetry without a timestamp
     void serialWriteTlm(HardwareSerial& serialInst, uint8_t id);
     void serialWriteTlm(Serial_& serialInst, uint8_t id);
-		
-		/*
-		 *	Other Functions
-		 */ 
-		 
-		void addTlm(uint8_t id, uint32_t* data);
-		 
-		void buildTlm(uint8_t id, String packet);
-		
+
+    // Write a telemetry packet with a given timestamp
+    void serialWriteTlm(HardwareSerial& serialInst, uint8_t id, uint32_t timeStamp);
+    void serialWriteTlm(Serial_& serialInst, uint8_t id, uint32_t timeStamp); 
+
+    // Parse a command that comes in over the serial monitor
+    void serialParseCmd(HardwareSerial& serialInst);
+    void serialParseCmd(Serial_& serialInst);
+				
 	private:
 
     /* Interfaces */
@@ -48,8 +70,11 @@ class coats
 		#else
 		uint32_t 	*packetPointers[256];
 		#endif
+    cmdCallback   cmdCallbackPtr[256];
 		size_t 	packetSizes[256];
 		uint16_t endString;
 		bool timerEnable;
+    bool statEnable;
+    
 
 };
